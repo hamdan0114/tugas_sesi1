0294f7c916670df0c8b804fcb789776612e39f05
hamdan0114 <147163660+hamdan0114@users.noreply.github.com>
2024-02-28 20:01:08 +0700

    Add files via upload

---
+++ sesi1/Main.py
@@ -0,0 +1,10 @@
+print("=== Aplikasi Biodata===")
+name = input ("masukian nama: ")
+gender = input ("masukan gender: ")
+tanggal_lahir = input ("masukan tanggal lahir: ")
+hobi = input ("masukan hobi: ")
+
+print("nama : ", name)
+print("gender : ", gender)
+print("tanggal lahir : ", tanggal_lahir)
+print("hobi: ", hobi)
